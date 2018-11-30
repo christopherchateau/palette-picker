c55407271f28bad484e14695e57b31c516a55800
christopherchateau <christopherchateau@gmail.com>
2018-11-29 17:03:19 -0700

    Add generateRandomHexCode and generateRandomValue funcs

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,22 @@
-const toggleLock = (e) => {
-  $(e.target).toggleClass('fa-lock-open')
-  $(e.target).toggleClass('fa-lock')
-}
+const toggleLock = e => {
+  $(e.target).toggleClass("fa-lock-open");
+  $(e.target).toggleClass("fa-lock");
+};
 
-$('.fas').on('click', toggleLock)
+const generateRandomValue = () => {
+  const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+  const randomIndex = Math.floor(Math.random() * 16);
+  return values[randomIndex];
+};
 
+const generateRandomHexCode = () => {
+  let hexCode = ''
+
+  while (hexCode.length < 6) {
+    hexCode += generateRandomValue();
+  }
+  console.log(hexCode);
+};
+
+$(".fas").on("click", toggleLock);
+generateRandomHexCode();
