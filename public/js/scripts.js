3a0f1b60d120af0e2eb2e7f69302c2b3ebaeba42
christopherchateau <christopherchateau@gmail.com>
2018-11-29 20:54:04 -0700

    Add generateRandomColors func and event listener to refresh colors btn

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,22 +1,35 @@
-const toggleLock = e => {
-  $(e.target).toggleClass("fa-lock-open");
-  $(e.target).toggleClass("fa-lock");
-};
+$(".fas").on("click", toggleLock);
+$(".refresh-colors-btn").on("click", generateRandomColors);
 
-const generateRandomValue = () => {
+function generateRandomColors() {
+  const colors = [];
+
+  while (colors.length < 5) {
+    colors.push(generateRandomHexCode());
+  }
+  for (let i = 1; i <= 5; i++) {
+    $(`.color-${i}`).css("background-color", colors[i - 1]);
+  }
+}
+
+function generateRandomHexCode() {
+  let hexCode = "#";
+
+  while (hexCode.length < 7) {
+    hexCode += generateRandomHexValue();
+  }
+  return hexCode;
+}
+
+function generateRandomHexValue() {
   const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
   const randomIndex = Math.floor(Math.random() * 16);
   return values[randomIndex];
-};
+}
 
-const generateRandomHexCode = () => {
-  let hexCode = ''
+function toggleLock(e) {
+  $(e.target).toggleClass("fa-lock-open");
+  $(e.target).toggleClass("fa-lock");
+}
 
-  while (hexCode.length < 6) {
-    hexCode += generateRandomValue();
-  }
-  console.log(hexCode);
-};
-
-$(".fas").on("click", toggleLock);
-generateRandomHexCode();
+generateRandomColors();
