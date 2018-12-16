8d0d62ad0bf854f3b87a7baeed9ee338aeee274a
Christopher Chateau <christopherchateau@gmail.com>
2018-12-16 13:33:55 -0700

    Remove seed from knexfile production env

--- knexfile.js
+++ knexfile.js
@@ -16,9 +16,6 @@ module.exports = {
     migrations: {
       directory: "./db/migrations"
     },
-    seeds: {
-      directory: "./db/seeds/production"
-    },
     useNullAsDefault: true
   }
 };
